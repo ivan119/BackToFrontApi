f88d62e12e668a84be8d111d7557308f524657f5
ivan119 <ivankelava91@gmail.com>
2020-02-27 15:09:49 +0100

    minor changes..

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -67,9 +67,7 @@ class UserController {
             response.ok({
               token
             })
-          }
-          
-     
+          }  
           
     }
 
--- start/routes.js
+++ start/routes.js
@@ -22,16 +22,15 @@ Route.get('/', () => {
 Route.group(()=>{
     Route.post('login','UserController.login')
     Route.post('register','UserController.register')
-    Route.delete('logout','UserController.logout')
+    Route.delete('logout','UserController.logout') // Do i even need this? //
     Route.get('getuser','UserController.show').middleware(["getUser"]) //Get one user//
     Route.get('index','UserController.index') //Get all users//
-    Route.get('getFavourite/:id','UserController.getFavourite')
+    Route.get('getFavourite/:id','UserController.getFavourite') //Get all favouirte movies by User id //
 }).prefix('users')
 
-Route.get('movies', 'MovieController.index')
-//Route.get('movies', 'MoviesController.getFavourite').middleware(["getUser"])
-Route.get('movies/:id', 'MovieController.show')
-Route.post('movies', 'MovieController.store')
-//Route.post('movies/:id', 'MovieController.favourite').middleware(["getUser"])
-Route.patch('movies/:id', 'MovieController.update').middleware(['findMovie'])
-Route.delete('movies/:id', 'MovieController.delete').middleware(['findMovie'])
+Route.get('movies', 'MovieController.index') // Get all movies from database //
+Route.get('movies/:id', 'MovieController.show') // Show singe movie by id //
+Route.post('movies', 'MovieController.store') 
+Route.post('movies/:id', 'MovieController.favourite').middleware(["getUser"]) // Post favourite movie id and user id to pivot table //
+Route.patch('movies/:id', 'MovieController.update').middleware(['findMovie']) // Update movie //
+Route.delete('movies/:id', 'MovieController.delete').middleware(['findMovie']) // Delete movie //
